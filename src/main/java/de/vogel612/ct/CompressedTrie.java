6f610d7120c526451a7313c394cbf8fb993abb94
Vogel612 <Vogel612@gmx.de>
2015-10-09 16:26:57 +0200

    Implemented the Collection interface for CompressedTrie

--- src/main/java/de/vogel612/ct/CompressedTrie.java
+++ src/main/java/de/vogel612/ct/CompressedTrie.java
@@ -3,10 +3,13 @@ package de.vogel612.ct;
 import static de.vogel612.ct.TrieNode.prefixMatching;
 
 import java.util.*;
+import java.util.function.BinaryOperator;
 
-public class CompressedTrie {
+public class CompressedTrie implements Collection<String> {
 
     private final TrieNode root = new TrieNode("");
+
+    private int size;
 
     public CompressedTrie() {
     }
@@ -15,32 +18,70 @@ public class CompressedTrie {
         addAll(items);
     }
 
-    public void add(String newString) {
+    public boolean add(String newString) {
+        if (contains(newString)) {
+            return false;
+        }
         root.addChild(newString);
+        size++;
+        return true;
     }
 
-    public void addAll(Collection<String> items) {
-        items.stream().forEach(this::add);
+    @Override
+    public boolean retainAll(Collection<?> collection) {
+        return false;
     }
 
-    public boolean remove(String word) {
-        return removeWord(word);
+    @Override
+    public void clear() {
+        removeAll(matches(""));
     }
 
-    public boolean removeAll(Collection<String> words) {
+    @Override
+    public boolean addAll(Collection<? extends String> items) {
+        return items.stream().map(this::add).reduce((b1, b2) -> b1 && b2).orElse(false);
+    }
+
+    @Override
+    public boolean remove(Object word) {
+        Objects.requireNonNull(word, "Cannot look for a null word");
+        if (!(word instanceof String)) {
+            throw new ClassCastException("Trie is only intended for Strings");
+        }
+        TrieNode wordNode = findWordNode((String)word, root);
+        if (wordNode != null) {
+            // FIXME purge orphaned childnodes
+            wordNode.isCompleteWord = false;
+            size--;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> words) {
         boolean result = true;
-        for (String word : words) {
+        for (Object word : words) {
             result &= remove(word);
         }
         return result;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        if (o instanceof String) {
+            return contains((String) o);
+        }
+        return false;
     }
 
     public boolean contains(String word) {
         return findWord(word);
     }
 
-    public boolean containsAll(Collection<String> words) {
-        for (String word : words) {
+    @Override
+    public boolean containsAll(Collection<?> words) {
+        for (Object word : words) {
             if (!contains(word)) {
                 return false;
             }
@@ -48,8 +89,16 @@ public class CompressedTrie {
         return true;
     }
 
-    public boolean containsAny(Collection<String> words) {
-        for (String word : words) {
+    /**
+     * Checks whether any of the items in the given collection is in the Trie
+     *
+     * @param words The collection to check for items
+     *
+     * @return A boolean if (and only if) for any item in the given collection i, an item o in the Trie exists so that
+     * <tt>o.equals(i)</tt>
+     */
+    public boolean containsAny(Collection<?> words) {
+        for (Object word : words) {
             if (contains(word)) {
                 return true;
             }
@@ -57,6 +106,14 @@ public class CompressedTrie {
         return false;
     }
 
+    /**
+     * Returns a list of matches from this Trie where a match is a word, that has the same prefix
+     * as the given prefix. There are no guarantees to the order of the list.
+     *
+     * @param prefix The prefix that the word has to begin with to be considered a match.
+     *
+     * @return A List of matches.
+     */
     public List<String> matches(String prefix) {
         Map<TrieNode, String> subtreeData = root.findMatchingSubtree("", prefix);
         TrieNode subtree = subtreeData.keySet().iterator().next(); // only one entry
@@ -85,17 +142,6 @@ public class CompressedTrie {
         return wordNode != null && wordNode.isCompleteWord;
     }
 
-    private boolean removeWord(final String word) {
-        Objects.requireNonNull(word, "Cannot look for a null word");
-        TrieNode wordNode = findWordNode(word, root);
-        if (wordNode != null) {
-            // FIXME purge orphaned childnodes
-            wordNode.isCompleteWord = false;
-            return true;
-        }
-        return false;
-    }
-
     private TrieNode findWordNode(final String word, final TrieNode tree) {
         Objects.requireNonNull(word, "Cannot look for a null word");
         if (word.equals(tree.prefix)) {
@@ -109,5 +155,30 @@ public class CompressedTrie {
             }
         }
         return null;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return matches("").iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return matches("").toArray();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] ts) {
+        return matches("").toArray(ts);
     }
 }
